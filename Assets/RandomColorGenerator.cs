ce5251c379c148e04b439c69256f3f4050091542
Valentino <valentino136@gmail.com>
2024-09-03 13:34:05 +0200

    Added random color generator + short pop up

---
+++ Assets/RandomColorGenerator.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomColorGenerator : MonoBehaviour
+{
+    public Image DisplayImage;
+    public float PopUpTime;
+
+    private Color RandomColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RandomColor = DisplayImage.color;
+
+        ColorPopUp();
+    }
+
+    public void ColorPopUp()
+    {
+        Color random = new Color(Random.value, Random.value, Random.value);
+
+        StartCoroutine(FlashColor(random));
+    }
+
+    private System.Collections.IEnumerator FlashColor(Color color)
+    {
+        // Set the Image's color to the random color.
+        DisplayImage.color = color;
+
+        // Wait for the specified flash duration.
+        yield return new WaitForSeconds(PopUpTime);
+
+        // Revert the Image's color back to the original.
+        DisplayImage.color = RandomColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
